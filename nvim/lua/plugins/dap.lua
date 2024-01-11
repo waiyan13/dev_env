e0c56a40f75ad65b211231e4645a8fc979291ce1
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-01-11 11:16:38 +0630

    feat[nvim]: Add debugger config

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -67,6 +67,7 @@ return {
                 name = "",
                 module = "",
                 args = {},
+                pythonPath = "",
                 cwd = "",
             })
             --]]
